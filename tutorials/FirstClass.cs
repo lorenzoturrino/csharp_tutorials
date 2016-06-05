b441e689ca1f269088ee71046a07c6785dd8c6a7
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-06-05 11:54:45 +0100

    add dude class

--- tutorials/FirstClass.cs
+++ tutorials/FirstClass.cs
@@ -2,14 +2,27 @@
 
 namespace tutorials
 {
+	public class Dude
+	{
+		private string greetings = "hello I'm dude";
+
+		public void printGreeting() 
+		{
+			System.Console.WriteLine (greetings);
+		}
+	}
+
 	public class FirstClass
 	{
-		static public void Main (string[] args)
+		static public int Main (string[] args)
 		{
 			System.Console.WriteLine ("hello world" + " you wrote {0} args", args.Length);
 			foreach(string arg in args) {
 				System.Console.WriteLine ("Arg is " + arg);
 			}
+			Dude guy = new Dude ();
+			guy.printGreeting ();
+			return 0;
 		}
 	}
 }
